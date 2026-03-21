97b408f337af9df2e950b875c5ad80208ee23c59
Tugrulinho <tugrulinho77@gmail.com>
2026-03-21 09:05:20 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1010,6 +1010,7 @@ function App() {
         <Route path="content" element={<AdminContent />} />
         <Route path="services" element={<AdminServices />} />
         <Route path="pricing" element={<AdminPricing />} />
+        <Route path="messages" element={<Messages />} />
       </Route>
 </Routes>
 </>
