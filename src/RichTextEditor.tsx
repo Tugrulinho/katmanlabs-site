94b831ffb51381fd11ba967bc1f3a2a425d770fe
Tugrulinho <tugrulinho77@gmail.com>
2026-03-27 02:49:20 +0300

    stiky yapıldı

--- src/RichTextEditor.tsx
+++ src/RichTextEditor.tsx
@@ -57,7 +57,7 @@ export default function RichTextEditor({
 
   return (
     <div className="border rounded bg-white overflow-hidden">
-      <div className="flex flex-wrap gap-2 p-2 border-b bg-gray-50">
+      <div className="sticky top-0 z-20 flex flex-wrap gap-2 p-2 border-b bg-gray-50">
         <button
           type="button"
           onClick={() => editor.chain().focus().toggleBold().run()}
