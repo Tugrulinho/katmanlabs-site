95617902b6fcbb92313d26c8f482dca02057856e
Tugrulinho <tugrulinho77@gmail.com>
2026-04-08 17:04:16 +0300

    otomatik blog çekme

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,23 +1,14 @@
+import BlogSection from "../components/BlogSection";
 import { useBlogs } from "../hooks/useBlogs";
 export default function Blog() {
   const { blogs, loading } = useBlogs();
   return (
-    <div className="max-w-7xl mx-auto px-4 py-20">
-      <h1>Blog</h1>
-
-      {loading ? (
-        <p>yükleniyor...</p>
-      ) : (
-        blogs.map((b) => (
-          <a
-            key={b.id}
-            href={`/blog/${b.slug}`}
-            className="block mb-3 text-lg text-gray-900 hover:text-primary"
-          >
-            {b.title}
-          </a>
-        ))
-      )}
-    </div>
+    <BlogSection
+      content={{}}
+      blogs={blogs}
+      loading={loading}
+      isMobile={false}
+      getBlogBadgeColor={() => "bg-primary"}
+    />
   );
 }
