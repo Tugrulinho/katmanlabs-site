df4a2c8d73bdef9ed5155f5f89061cbdd5d1646f
Tugrulinho <tugrulinho77@gmail.com>
2026-04-08 17:20:26 +0300

    blog sayfası

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -4,13 +4,32 @@ import Footer from "../components/Footer";
 export default function Blog() {
   const { blogs, loading } = useBlogs();
   return (
-    <BlogSection
-      content={{}}
-      blogs={blogs}
-      loading={loading}
-      isMobile={false}
-      getBlogBadgeColor={() => "bg-primary"}
-    />
+    <>
+      <section className="pt-24 pb-16">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-10">
+            {/* Sol alan - blog listesi */}
+            <div className="lg:col-span-8">
+              <BlogSection
+                content={{}}
+                blogs={blogs}
+                loading={loading}
+                isMobile={false}
+                getBlogBadgeColor={() => "bg-primary"}
+              />
+            </div>
+
+            {/* Sağ alan - sidebar */}
+            <aside className="lg:col-span-4">
+              <div className="rounded-2xl border border-white/10 p-6 sticky top-24">
+                Sidebar gelecek
+              </div>
+            </aside>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </>
   );
-  <Footer />;
 }
